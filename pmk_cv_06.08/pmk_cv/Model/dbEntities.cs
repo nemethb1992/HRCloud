601516156f1644d2c4ef2a92cb02cf9d75adb87f
Németh Balázs <39947176+nemethb1992@users.noreply.github.com>
2018-06-22 21:47:57 +0200

    commit

--- pmk_cv_06.08/pmk_cv/Model/dbEntities.cs
+++ pmk_cv_06.08/pmk_cv/Model/dbEntities.cs
@@ -29,7 +29,7 @@ namespace HRCloud.Model
         //Initialize values
         private void SetupDB()
         {
-            string connectionString = "Data Source = 192.168.144.189; Port=3306; Initial Catalog = pmkcvtest; User ID=hr-admin; Password=pmhr2018";
+            string connectionString = "Data Source = s7.nethely.hu; Initial Catalog = pmkcvtest; User ID=pmkcvtest; Password=pmkcvtest2018";
             conn = new MySqlConnection(connectionString);
         }
         public bool dbOpen()
